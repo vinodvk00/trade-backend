7aefed252100e8110e0f23a24f7355746268802d
vinod <21r21a3344@mlrinstitutions.ac.in>
2025-12-20 23:27:30 +0530

    chore: include testing and script in lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ const prettierConfig = require('eslint-config-prettier');
 
 module.exports = [
   {
-    ignores: ['node_modules', 'dist', 'coverage', '*.config.js', 'scripts/**', 'tests/**']
+    ignores: ['node_modules', 'dist', 'coverage', '*.config.js']
   },
   {
     files: ['src/**/*.ts'],
@@ -27,9 +27,41 @@ module.exports = [
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false
+        }
+      ],
+      'no-unused-vars': 'off',
+      'no-console': 'off'
+    }
+  },
+  {
+    files: ['tests/**/*.ts', 'scripts/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
           caughtErrorsIgnorePattern: '^_'
         }
       ],
+      'no-unused-vars': 'off',
       'no-console': 'off'
     }
   }
